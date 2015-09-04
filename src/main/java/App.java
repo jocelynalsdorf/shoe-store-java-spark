cd5ba707339998358f59ec81c42cb1d0f46ab716
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-04 09:21:49 -0700

    add routes/views for adding brand to store and store to brand

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -34,6 +34,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+//post routes to make a new brand or store
      post("/brands", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
         String description = request.queryParams("description");
@@ -49,6 +50,48 @@ public class App {
         Store newStore = new Store(name);
         newStore.save();
         response.redirect("/stores");
+        return null;
+      });
+
+//get routes to individual store/brand pages 
+      get("/brands/:id", (request,response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        int id = Integer.parseInt(request.params("id"));
+        Brand brand = Brand.find(id);
+        model.put("brand", brand);
+        model.put("allStores", Store.all());
+        model.put("template", "templates/brand.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/stores/:id", (request,response) ->{
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        int id = Integer.parseInt(request.params("id"));
+        Store store = Store.find(id);
+        model.put("store", store);
+        model.put("allBrands", Brand.all());
+        model.put("template", "templates/store.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+//post routes to add brand to stores and add stores to brands
+       post("/add_brands", (request, response) -> {
+        int brandId = Integer.parseInt(request.queryParams("brand_id"));
+        int storeId = Integer.parseInt(request.queryParams("store_id"));
+        Store store = Store.find(storeId);
+        Brand brand = Brand.find(brandId);
+        store.addBrand(brand);
+        response.redirect("/stores/" + storeId);
+        return null;
+      });
+
+      post("/add_stores", (request, response) -> {
+        int brandId = Integer.parseInt(request.queryParams("brand_id"));
+        int storeId = Integer.parseInt(request.queryParams("store_id"));
+        Store store = Store.find(storeId);
+        Brand brand = Brand.find(brandId);
+        brand.addStore(store);
+        response.redirect("/brands/" + brandId);
         return null;
       });
 
