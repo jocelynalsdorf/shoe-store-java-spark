32d5e7ad5a6d62b1252cef43713278e60d11afc5
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-04 08:40:12 -0700

    changes getBrands methods to utilize join statement; junit tests still passing

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -65,25 +65,35 @@ public class Store {
     }
   }
 
-  public ArrayList<Brand> getBrands() {
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "SELECT brand_id FROM stores_brands WHERE store_id = :store_id";
-      List<Integer> brandIds = con.createQuery(sql)
-        .addParameter("store_id", this.getId())
-        .executeAndFetch(Integer.class);
-
-      ArrayList<Brand> brands = new ArrayList<Brand>();
-
-      for (Integer brandId : brandIds) {
-        String brandQuery = "SELECT * FROM brands WHERE id = :brandId";
-        Brand brand = con.createQuery(brandQuery)
-          .addParameter("brandId", brandId)
-          .executeAndFetchFirst(Brand.class);
-        brands.add(brand);
-      }
-      return brands;
+  public List<Brand> getBrands() {
+  try(Connection con = DB.sql2o.open()){
+    String sql = "SELECT brands.* FROM stores JOIN stores_brands ON (stores.id = stores_brands.store_id) JOIN brands ON (stores_brands.brand_id = brands.id) where stores.id= :store_id";
+       List<Brand> brands = con.createQuery(sql)
+      .addParameter("store_id", this.getId())
+      .executeAndFetch(Brand.class);
+    return brands;
     }
   }
+
+  // public ArrayList<Brand> getBrands() {
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT brand_id FROM stores_brands WHERE store_id = :store_id";
+  //     List<Integer> brandIds = con.createQuery(sql)
+  //       .addParameter("store_id", this.getId())
+  //       .executeAndFetch(Integer.class);
+
+  //     ArrayList<Brand> brands = new ArrayList<Brand>();
+
+  //     for (Integer brandId : brandIds) {
+  //       String brandQuery = "SELECT * FROM brands WHERE id = :brandId";
+  //       Brand brand = con.createQuery(brandQuery)
+  //         .addParameter("brandId", brandId)
+  //         .executeAndFetchFirst(Brand.class);
+  //       brands.add(brand);
+  //     }
+  //     return brands;
+  //   }
+  // }
 
   public void delete() {
     try(Connection con = DB.sql2o.open()) {
